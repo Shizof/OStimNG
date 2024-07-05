287a98e138eef1b399d52e06fa2a4c669e1bc089
Shizof <shizof@gmail.com>
2024-07-06 01:03:57 +0300

    Ostim VR 7.3.1.1 update version info change.

--- skse/src/Main.cpp
+++ skse/src/Main.cpp
@@ -145,7 +145,7 @@ namespace {
 
 extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
     SKSE::PluginVersionData v;
-    v.PluginVersion(REL::Version("7.3.1.0"sv));
+    v.PluginVersion(REL::Version("7.3.1.1"sv));
     v.PluginName("OStim");
     v.AuthorName("VersuchDrei");
     v.UsesAddressLibrary(true);
@@ -157,7 +157,7 @@ extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info) {
     a_info->infoVersion = SKSE::PluginInfo::kVersion;
     a_info->name = "OStim";
-    a_info->version = 0x07030010;
+    a_info->version = 0x07030011;
 
     return true;
 }
